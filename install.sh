6f13aaf04ad585545eb875ad103c804ed1200bfd
Juan Alvarez <jfhack7@gmail.com>
2022-10-24 23:47:48 -0300

    message about chsh password and feature to not make fish default shell

--- install.sh
+++ install.sh
@@ -5,15 +5,20 @@ export UPDATE=0
 source util.sh
 
 install_packages git fish curl
-if [[ "$(basename $SHELL)" == "fish" ]]; then
+
+if [[ "$(basename $SHELL)" == "fish" || ! -z "$ALREADY_USING_FISH" ]]; then
   echo "I: already using fish"
 else
   if [[ $TERMUX == 1 ]]; then
     chsh -s fish
   else
+    if [[ $EUID > 0 ]]; then
+      echo "I: password might be required by: chsh -s $(which fish)"
+    fi
     chsh -s $(which fish)
   fi
 fi
+
 mkdir -p ~/.config/fish/functions
 cp fish/config.fish ~/.config/fish/config.fish
 cp fish/fish_title.fish ~/.config/fish/functions/fish_title.fish
